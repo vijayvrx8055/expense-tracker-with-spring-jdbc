a9e9d379cc38d1090ff99ff711b78b5e322fda1f
Vijay Kumar <118923157+vijayvrx8055@users.noreply.github.com>
2024-05-01 19:46:54 +0530

    inital code added

---
+++ expense-tracker-app/src/main/java/com/vrx/tracker/ExpenseTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.vrx.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerAppApplication.class, args);
+	}
+
+}
---
+++ expense-tracker-app/src/test/java/com/vrx/tracker/ExpenseTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrx.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
